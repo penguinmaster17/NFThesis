eac280a57b144ba669e8dbf51d02aed5f04ea60d
ktdykstra <ktdykstra@gmail.com>
2022-04-06 22:05:52 -0400

    adjusting dotenv call in pinata file

--- src/utils/pinata.js
+++ src/utils/pinata.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
 const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
 
