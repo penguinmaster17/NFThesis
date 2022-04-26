7c204359d1fc4a7a91adea9782626268547fef51
Kasey McFadden <kaseysmcfadden@gmail.com>
2022-04-25 20:50:33 -0400

    Test Heroku deployment

--- src/Minter.js
+++ src/Minter.js
@@ -81,7 +81,7 @@ const Minter = (props) => {
       <br></br>
       <h1 id="title">NFThesis</h1>
       <p>
-        Simply add your asset's link, name, and description, then press "Mint."
+        TESTING TESTING Simply add your asset's link, name, and description, then press "Mint."
       </p>
       <form>
         <h2>Link to asset: </h2>
