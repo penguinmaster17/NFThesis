d0b8624809bb7b6e8ce0000ed3debee38124b0f3
Aliya Ismagilova <aliyai@princeton.edu>
2022-04-28 00:06:12 -0400

    auth0 init

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+<Auth0Provider
+    domain="dev-9xppkwat.us.auth0.com"
+    clientId="lQJUhKn9bN3vqaas1DErpytCjg36k9R9"
+    redirectUri={window.location.origin}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +22,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
