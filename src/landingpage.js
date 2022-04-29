40ebfc4903df23df9afb0a31998dd96617e78c76
Aliya Ismagilova <aliyai@princeton.edu>
2022-04-29 17:00:15 -0400

    log in restriction test

--- src/landingpage.js
+++ src/landingpage.js
@@ -3,12 +3,15 @@ import LoginButton from "./login"
 const LandingPage = () => {
     return (
         <div className="Minter">
-        <div style={{display: 'flex', justifyContent: 'center', height: '50vh', alignItems: 'center', alignContent: 'center'}}>
+        
+        <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', height: '50vh', alignItems: 'center', alignContent: 'center'}}>
+    
     <div style={{display: 'flex', justifyContent: 'center', width: '50%'}}>
     <LoginButton />
+    <br />
     </div>
+    <div style={{color: 'red', margin: '20px' }}>You must log in with your Princeton email address! 🐯</div>  
     </div>
-            
         </div>
     );  
 }
